f4ccd6a8b7e4647b34bec95d80150b9858c7c539
eshagaikwad <74804285+eshagaikwad@users.noreply.github.com>
2021-06-30 13:05:39 +0530

    output updated

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-var btntranslate=document.querySelector("#btn-translate");
-var txtinput=document.querySelector("#txt-input");
-var outputdiv=document.querySelector("#output");
-
-
-
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 console.log(txtInput)
 
 function clickHandler() {
     console.log("clicked!");
     console.log("input", txtInput.value);
-   outputDiv.innerText = "ajsjsjsjsjsjsj  " + txtInput.value;
+   outputDiv.innerText = "hhhh  " + txtInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
 
 
- 
-
-
+   
